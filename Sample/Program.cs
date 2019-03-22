050f2a7db83d9290cff921d73c49f4d4797a52ef
George Plotnikov <accembler@gmail.com>
2019-03-22 10:28:27 +0300

    add output method info
    added verbose method flags dump and updated sample to show how it works

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -8,6 +8,43 @@ using System.Runtime.InteropServices;
 
 namespace Sample
 {
+    abstract class AbstractSampleClass
+    {
+        public abstract int Calc(int x, int y);
+    }
+
+    class SampleClass : AbstractSampleClass
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public sealed override int Calc(int x, int y)
+        {
+            var r = Math.Asin((double)x);
+            return (int)r * y * 1;
+        }
+    }
+    
+    class SampleClass1
+    {
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public virtual int Calc(double x, int y)
+        {
+            var r = Math.Asin((double)x);
+            return (int)r * y * 2;
+        }
+    }
+
+    class SampleClass2
+    {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Calc(int x, decimal y)
+        {
+            var r = Math.Asin((double)x);
+            return (int)r * y.CompareTo(x) * 3;
+        }
+    }
+
     unsafe class Program
     {
         static void Main(string[] args)
@@ -16,13 +53,20 @@ namespace Sample
             Console.WriteLine($"Last Win32 exception: {new Win32Exception(Marshal.GetLastWin32Error()).Message}");
             try
             {
-                using (var fs = new FileStream("./Result.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                using (var fs = new FileStream("./Result.txt", FileMode.OpenOrCreate, FileAccess.Write))
                 using (var sw = new StreamWriter(fs))
                 {
                     Win32IOUtils.SetStdHandle(Win32IOUtils.STD_OUTPUT_HANDLE, fs.SafeFileHandle.DangerousGetHandle());
                     var cmi = new CompiledMethodInfo<Program>();
                     cmi.SetUp();
-                    Calc(0x0fffffff, 0x11111111);
+                    var sample = new SampleClass();
+                    cmi.SetUp();
+                    Console.WriteLine(sample.Calc(0x0fffffff, 0x11111111));
+                    var sample1 = new SampleClass1();
+                    cmi.SetUp();
+                    Console.WriteLine(sample1.Calc(0x0fffffff, 0x11111111));
+                    cmi.SetUp();
+                    Console.WriteLine(SampleClass2.Calc(0x0fffffff, 0x11111111));
                     cmi.Release();
                 }
                 Console.OpenStandardOutput().Flush();
@@ -34,13 +78,6 @@ namespace Sample
                 Console.WriteLine($"Win32 exception: {new Win32Exception(Marshal.GetLastWin32Error()).Message}");
                 Console.ReadKey();
             }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static int Calc(int x, int y)
-        {
-            var r = Math.Asin((double)x);
-            return (int)r * y;
         }
     }
 }
